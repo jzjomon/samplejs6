0f2f4f410550739d64adc0118a6c2f42248a02c3
jomon jose <jzjomon@gmail.com>
2023-06-10 00:33:20 +0530

    initial commit

---
+++ arrayMethods.js/sort.js
@@ -0,0 +1,6 @@
+let arr1 = [98,87,76,65,54,43,32,21];
+console.log(arr1);
+console.log(arr1.sort());
+let arr2 = ["john","lewis","aron","james","smith"];
+console.log(arr2);
+console.log(arr2.sort());

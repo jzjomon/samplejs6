8176329752f4260f3103db6b60cef9d88c888930
jomon jose <jzjomon@gmail.com>
2023-06-10 16:51:22 +0530

    map and redcue added

---
+++ arrayMethods.js/map.js
@@ -0,0 +1,6 @@
+let arr = [98,87,76,65,54,43,32,21];
+let newarr = arr.map((ele)=>ele+10)
+console.log(newarr);
+let arr1 = [1,2,3,4,5]
+newarr = arr1.map((ele)=>ele*2)
+console.log(newarr);
---
+++ arrayMethods.js/reduce.js
@@ -0,0 +1,6 @@
+let arr1 = [98,87,76,65,54,43,32,21];
+let newarr = arr1.reduce((total,value)=>total+value);
+console.log(newarr);
+let arr2 = [1,2,3,4,5,6]
+let newarr2 = arr2.reduce((total,value)=>total*value);
+console.log(newarr2);
